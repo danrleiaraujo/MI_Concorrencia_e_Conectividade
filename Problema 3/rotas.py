4c4b8deda485097a60dca399ce51075790ece391
Danrlei Almeida Araujo <danrleiaraujo@gmail.com>
2022-12-06 21:02:33 -0300

    login

--- Problema 3/rotas.py
+++ Problema 3/rotas.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from static.usuarios import *
+import ast
 
 app = Flask(__name__)
 
@@ -12,6 +14,22 @@ def index():
 #Página de login
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
+    #Recebimento de dados:
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    verifica = verificaUser(usuario, senha)
+    if(verifica):
+        return logado(usuario, senha)
     return render_template ('login.html')
+
+# ---------------------------------------
+#Página de logado
+@app.route("/logado/<adm>/<senha>", methods = ['GET'])
+def logado(adm, senha):
+    verifica = verificaUser(adm, senha)
+    if(verifica):
+        return render_template ('indexLogado.html', user = adm)
+    else:
+        return login()
 
 app.run(debug=True)
---
+++ Problema 3/static/usuarios.py
@@ -0,0 +1,33 @@
+adm = {   
+    1:{
+        "nome": "Danrlei_Araujo",
+        "senha": "1010"
+    },
+    2:{
+        "nome": "Evelyn_Suzart",
+        "senha": "1212"
+    }
+}
+
+#Retorna a lista:
+def getAdm():
+    global adm
+    return adm
+
+#Adiciona:
+def adicionarAdm(usuario, senha):
+    global adm
+    final = len(adm) + 1 
+    novo = {"nome":usuario, "senha":senha}
+    adm[final] = novo
+
+#Verifica se existe:
+def verificaUser(usuario, senha):
+    global adm
+    administradores = getAdm()
+    for user in administradores:
+        admin = administradores.get(user)
+        if(usuario == admin.get("nome") and senha == admin.get("senha")):
+            return True
+    return False
+    #Caso contrário:
